02abbd3318eb30413e43329594a8497b0ed86f10
Rohit Mohan <rohitmohan96@gmail.com>
2015-05-26 21:32:27 +0530

    Minor changes

--- src/main/java/com/rohit/test/TestClass.java
+++ src/main/java/com/rohit/test/TestClass.java
@@ -39,13 +39,6 @@ public class TestClass {
 		for (int i = a; i <= b; i++) {
 			b = 6;
 		}
-		
-		for(int i = 0; i < 5; i++) {
-			for(int j = 0; j < 5; j++) {
-				System.out.print(i + ", " + j + "\t");
-			}
-			System.out.println();
-		}
 
 	}
 }
