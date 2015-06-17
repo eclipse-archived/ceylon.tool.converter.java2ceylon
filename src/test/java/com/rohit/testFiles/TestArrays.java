f990dce7e8a40e16258cb7a5cb79e2f1f2d4bff0
Rohit Mohan <rohitmohan96@gmail.com>
2015-06-18 02:34:43 +0530

    Modified tests

--- src/test/java/com/rohit/testFiles/TestArrays.java
+++ src/test/java/com/rohit/testFiles/TestArrays.java
@@ -2,10 +2,12 @@ package com.rohit.testFiles;
 
 public class TestArrays {
 	public void foo() {
-		int[] a = new int[5];
+		int[] a = new int[5];	
 		
 		int b;
 		a[2] = 6;
 		b = a[4];
+		
+		TestClass[] c = new TestClass[b];
 	}
 }
--- src/test/java/com/rohit/testFiles/testArrays.ceylon
+++ src/test/java/com/rohit/testFiles/testArrays.ceylon
@@ -4,5 +4,6 @@ variable IntArray a = IntArray(5);
 variable Integer b;
 a.set(2, 6);
 b=a.get(4);
+variable ObjectArray<TestClass> c = ObjectArray<TestClass>(b);
 }
 }
