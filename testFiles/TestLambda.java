9fe484da8f5c605a7952f8a6a9ef7ad17cc667c9
Bastien Jansen <bastien.jansen@gmx.com>
2016-11-21 13:54:00 +0100

    Additional tests for #23.

--- testFiles/TestLambda.java
+++ testFiles/TestLambda.java
@@ -3,5 +3,8 @@ class Lamba {
         Runnable hello = () -> { System.out.println("hello"); };
 
         swingComponent.addActionListener(event -> System.out.println("event handler"));
+        swingComponent.addActionListener((event) -> System.out.println("event handler"));
+        swingComponent.addActionListener((Event event) -> System.out.println("event handler"));
+        Collections.sort(strings, (s1, s2)-> s1.compareTo(s2));
     }
 }
--- testFiles/testLambda.ceylon
+++ testFiles/testLambda.ceylon
@@ -6,6 +6,9 @@ print("hello");
 }
 ;
 swingComponent.addActionListener((event) => print("event handler"));
+swingComponent.addActionListener((event) => print("event handler"));
+swingComponent.addActionListener((Event event) => print("event handler"));
+Collections.sort(strings, (s1, s2) => s1.compareTo(s2));
 }
 
 }
